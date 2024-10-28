cba233eaa07c7fb93100d8e9156ef6acfa750412
Ericles Willian <ewnsilva@gmail.com>
2024-10-28 20:42:58 -0300

    refactor: initial mock

---
+++ frontend/src/utils/Mocks.ts
@@ -0,0 +1,73 @@
+import { FoodExemple } from "assets";
+
+export const ProductMock = [
+  {
+    image: FoodExemple,
+    name: "Classic Burger",
+    description:
+      "Um hambúrguer tradicional com carne suculenta, alface, tomate e molho especial.",
+    price: 19.99,
+  },
+  {
+    image: FoodExemple,
+    name: "Cheese Lover",
+    description:
+      "Hambúrguer com dupla camada de queijo cheddar e carne grelhada no ponto certo.",
+    price: 22.99,
+  },
+  {
+    image: FoodExemple,
+    name: "Bacon Deluxe",
+    description:
+      "Carne grelhada com fatias crocantes de bacon, queijo e molho barbecue.",
+    price: 24.99,
+  },
+  {
+    image: FoodExemple,
+    name: "Veggie Delight",
+    description:
+      "Hambúrguer vegetariano com um mix de grãos, queijo, alface e guacamole.",
+    price: 18.99,
+  },
+  {
+    image: FoodExemple,
+    name: "Spicy Jalapeño",
+    description:
+      "Hambúrguer apimentado com jalapeños, pimenta caiena e molho picante.",
+    price: 21.99,
+  },
+  {
+    image: FoodExemple,
+    name: "Mushroom Swiss",
+    description:
+      "Hambúrguer com cogumelos salteados, queijo suíço e cebolas caramelizadas.",
+    price: 23.49,
+  },
+  {
+    image: FoodExemple,
+    name: "Chicken Crispy",
+    description:
+      "Delicioso hambúrguer de frango empanado, alface, tomate e maionese.",
+    price: 20.99,
+  },
+  {
+    image: FoodExemple,
+    name: "Double Trouble",
+    description:
+      "Dois hambúrgueres suculentos, queijo duplo, bacon e molho especial.",
+    price: 26.99,
+  },
+  {
+    image: FoodExemple,
+    name: "BBQ Ranch",
+    description: "Hambúrguer com molho ranch, queijo cheddar e cebolas crispy.",
+    price: 22.49,
+  },
+  {
+    image: FoodExemple,
+    name: "Blue Cheese Bomb",
+    description:
+      "Hambúrguer com queijo gorgonzola, cebolas roxas e molho de alho.",
+    price: 24.99,
+  },
+];
