d133d6a8e3c649992e23e0a008cadc6ca08379a1
Ericles Willian <ewnsilva@gmail.com>
2024-11-10 18:56:29 -0300

    feat: product increment

--- frontend/src/components/Product/index.tsx
+++ frontend/src/components/Product/index.tsx
@@ -5,15 +5,32 @@ import {
   CardMedia,
   CardContent,
   Grid2 as Grid,
+  IconButton,
 } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 import { IProductsList } from "context";
 import { useCart } from "hooks";
 
 export const ProductCard: React.FC<IProductsList> = ({ item }) => {
-  const { addToCart, cartItems } = useCart();
+  const {
+    addToCart,
+    isInCart,
+    incrementQuantity,
+    decrementQuantity,
+    cartItems,
+  } = useCart();
+
+  const cartItem = cartItems.find((cartItem) => cartItem.id === item.id);
+  const quantity = cartItem?.quantity || 0;
+
   return (
-    <Card sx={{ height: 450, display: "flex", flexDirection: "column" }}>
+    <Card
+      key={item.id}
+      sx={{ height: 450, display: "flex", flexDirection: "column" }}
+    >
       <CardMedia
         component="img"
         height="140"
@@ -39,7 +56,7 @@ export const ProductCard: React.FC<IProductsList> = ({ item }) => {
               variant="body2"
               color="text.secondary"
               sx={{
-                fontSize: { xs: "0.75rem", sm: "0,9rem" },
+                fontSize: { xs: "0.75rem", sm: "0.9rem" },
                 height: 70,
                 my: 2,
               }}
@@ -47,18 +64,59 @@ export const ProductCard: React.FC<IProductsList> = ({ item }) => {
               {item.description}
             </Typography>
           </Grid>
-          <Grid display={"flex"} flexDirection={"column"}>
+          <Grid display="flex" flexDirection="column">
             <Typography variant="h6" color="text.primary" sx={{ mt: 2 }}>
               ${item.price.toFixed(2)}
             </Typography>
-            <Button
-              variant="contained"
-              onClick={() => addToCart(item)}
-              color="primary"
-              sx={{ mt: 2 }}
-            >
-              Comprar
-            </Button>
+            {isInCart(item.id) ? (
+              <Grid
+                container
+                alignItems="center"
+                justifyContent={"space-around"}
+                spacing={1}
+                sx={{ mt: 2 }}
+              >
+                <Grid>
+                  {quantity === 1 ? (
+                    <IconButton
+                      onClick={() => decrementQuantity(item.id)}
+                      color="error"
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  ) : (
+                    <IconButton
+                      onClick={() => decrementQuantity(item.id)}
+                      color="error"
+                    >
+                      <RemoveIcon />
+                    </IconButton>
+                  )}
+                </Grid>
+
+                <Grid>
+                  <Typography variant="body1">{quantity}</Typography>
+                </Grid>
+
+                <Grid>
+                  <IconButton
+                    onClick={() => incrementQuantity(item.id)}
+                    color="primary"
+                  >
+                    <AddIcon />
+                  </IconButton>
+                </Grid>
+              </Grid>
+            ) : (
+              <Button
+                variant="contained"
+                onClick={() => addToCart(item)}
+                color="primary"
+                sx={{ mt: 2 }}
+              >
+                Comprar
+              </Button>
+            )}
           </Grid>
         </Grid>
       </CardContent>
