5dcfd8e2c952b8e74ecf3566f036b277da5da455
Ericles Willian <ewnsilva@gmail.com>
2024-11-10 18:56:50 -0300

    feat: products filter

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,7 +1,15 @@
-import { Fastfood } from "@mui/icons-material";
-import { Typography } from "@mui/material";
+import { ChangeEvent } from "react";
 
-export const Header = (): JSX.Element => {
+import { Fastfood, Search } from "@mui/icons-material";
+import { Box, InputAdornment, TextField, Typography } from "@mui/material";
+
+import { IHeader } from "context";
+
+export const Header = ({ setSearch }: IHeader): JSX.Element => {
+  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <header
       style={{
@@ -10,12 +18,40 @@ export const Header = (): JSX.Element => {
         flexDirection: "row",
         alignItems: "center",
         marginLeft: 10,
+        justifyContent: "space-between",
+        backgroundColor: "#fff4e0",
       }}
     >
-      <Fastfood fontSize="large" />
-      <Typography variant="h4" ml={1}>
-        Retro-Burgers
-      </Typography>
+      <Box display="flex" ml={"2%"} paddingY={2}>
+        <Fastfood htmlColor="#FA558A" fontSize="large" />
+        <Typography
+          variant="h4"
+          ml={1}
+          sx={{
+            backgroundImage: `radial-gradient(50% 50% at 50% 50%, #250F40 0%, #FA558A 100%)`,
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            fontWeight: "bold",
+            display: "inline-block",
+          }}
+        >
+          Retro-Burgers
+        </Typography>
+      </Box>
+      <TextField
+        label="Pesquisa"
+        variant="outlined"
+        size="small"
+        onChange={handleSearchChange}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Search />
+            </InputAdornment>
+          ),
+        }}
+        sx={{ mr: "5%" }}
+      />
     </header>
   );
 };
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,29 +1,37 @@
-import { Box, Grid2 as Grid } from "@mui/material";
-import { Header, Navigation, ProductCard } from "components";
+import { useState } from "react";
 
+import { Box, Grid2 as Grid } from "@mui/material";
+
+import { Cart, Header, Navigation, ProductCard, CartButton } from "components";
+import { useCart } from "hooks";
 import { ProductMock } from "utils";
 
-export const Home = () => (
-  <Box display="flex" flexDirection={"column"}>
-    <Header />
-    <Navigation />
-    <Grid
-      container
-      rowSpacing={2}
-      columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-      marginX={"15%"}
-    >
-      {ProductMock.map(({ description, image, name, price }) => (
-        <Grid size={3}>
-          <ProductCard
-            id={0}
-            description={description}
-            image={image}
-            name={name}
-            price={price}
-          />
-        </Grid>
-      ))}
-    </Grid>
-  </Box>
-);
+export const Home = () => {
+  const { totalQuantity } = useCart();
+  const [search, setSearch] = useState("");
+
+  const filteredProducts = ProductMock.filter((product) =>
+    product.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <Box display="flex" flexDirection={"column"}>
+      <Header setSearch={setSearch} />
+      <Navigation />
+      {totalQuantity > 0 && <CartButton />}
+      <Cart />
+      <Grid
+        container
+        rowSpacing={2}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        marginX={"15%"}
+      >
+        {filteredProducts.map((item) => (
+          <Grid key={item.id} size={3}>
+            <ProductCard item={item} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
