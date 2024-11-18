b8e6c22edaf588d57fbecd9fce14c714a1170b1f
Ericles Willian <ewnsilva@gmail.com>
2024-11-17 21:02:00 -0300

    refactor: adding new mocks

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,3 +1,6 @@
-import FoodExemple from "./Images/HamburgerExemple.jpg";
+import BurguerExample from "./Images/BurguerExample.jpg";
+import DrinkExample from "./Images/DrinkExample.jpg";
+import SnackExample from "./Images/SnackExample.jpg";
+import SweetExample from "./Images/SweetExample.jpg";
 
-export { FoodExemple };
+export { BurguerExample, DrinkExample, SnackExample, SweetExample };
--- frontend/src/utils/Mocks.ts
+++ frontend/src/utils/Mocks.ts
@@ -1,9 +1,14 @@
-import { FoodExemple } from "assets";
+import {
+  BurguerExample,
+  DrinkExample,
+  SnackExample,
+  SweetExample,
+} from "assets";
 
-export const ProductMock = [
+export const BurguerMock = [
   {
     id: 1,
-    image: FoodExemple,
+    image: BurguerExample,
     name: "Classic Burger",
     description:
       "Um hambúrguer tradicional com carne suculenta, alface, tomate e molho especial.",
@@ -11,7 +16,7 @@ export const ProductMock = [
   },
   {
     id: 2,
-    image: FoodExemple,
+    image: BurguerExample,
     name: "Cheese Lover",
     description:
       "Hambúrguer com dupla camada de queijo cheddar e carne grelhada no ponto certo.",
@@ -19,7 +24,7 @@ export const ProductMock = [
   },
   {
     id: 3,
-    image: FoodExemple,
+    image: BurguerExample,
     name: "Bacon Deluxe",
     description:
       "Carne grelhada com fatias crocantes de bacon, queijo e molho barbecue.",
@@ -27,7 +32,7 @@ export const ProductMock = [
   },
   {
     id: 4,
-    image: FoodExemple,
+    image: BurguerExample,
     name: "Veggie Delight",
     description:
       "Hambúrguer vegetariano com um mix de grãos, queijo, alface e guacamole.",
@@ -35,7 +40,7 @@ export const ProductMock = [
   },
   {
     id: 5,
-    image: FoodExemple,
+    image: BurguerExample,
     name: "Spicy Jalapeño",
     description:
       "Hambúrguer apimentado com jalapeños, pimenta caiena e molho picante.",
@@ -43,7 +48,7 @@ export const ProductMock = [
   },
   {
     id: 6,
-    image: FoodExemple,
+    image: BurguerExample,
     name: "Mushroom Swiss",
     description:
       "Hambúrguer com cogumelos salteados, queijo suíço e cebolas caramelizadas.",
@@ -51,7 +56,7 @@ export const ProductMock = [
   },
   {
     id: 7,
-    image: FoodExemple,
+    image: BurguerExample,
     name: "Chicken Crispy",
     description:
       "Delicioso hambúrguer de frango empanado, alface, tomate e maionese.",
@@ -59,25 +64,187 @@ export const ProductMock = [
   },
   {
     id: 8,
-    image: FoodExemple,
+    image: BurguerExample,
     name: "Double Trouble",
     description:
       "Dois hambúrgueres suculentos, queijo duplo, bacon e molho especial.",
     price: 26.99,
   },
+];
+
+export const SnackMock = [
   {
     id: 9,
-    image: FoodExemple,
-    name: "BBQ Ranch",
-    description: "Hambúrguer com molho ranch, queijo cheddar e cebolas crispy.",
-    price: 22.49,
+    image: SnackExample,
+    name: "Petisco de Carne",
+    description: "Espeto com carnes e linçuiças temperadas.",
+    price: 5.99,
   },
   {
     id: 10,
-    image: FoodExemple,
-    name: "Blue Cheese Bomb",
-    description:
-      "Hambúrguer com queijo gorgonzola, cebolas roxas e molho de alho.",
-    price: 24.99,
+    image: SnackExample,
+    name: "Quibe",
+    description: "Quibe recheado com carne moída temperada e hortelã.",
+    price: 4.99,
+  },
+  {
+    id: 11,
+    image: SnackExample,
+    name: "Pastel de Queijo",
+    description: "Pastel crocante com recheio de queijo derretido.",
+    price: 6.49,
+  },
+  {
+    id: 12,
+    image: SnackExample,
+    name: "Bolinha de Queijo",
+    description: "Bolinha de queijo cremosa com massa leve.",
+    price: 4.49,
+  },
+  {
+    id: 13,
+    image: SnackExample,
+    name: "Esfiha de Carne",
+    description: "Esfiha aberta com recheio de carne temperada.",
+    price: 7.99,
+  },
+  {
+    id: 14,
+    image: SnackExample,
+    name: "Empada de Frango",
+    description: "Empada recheada com frango desfiado e temperado.",
+    price: 6.99,
+  },
+  {
+    id: 15,
+    image: SnackExample,
+    name: "Croissant de Presunto e Queijo",
+    description: "Croissant folhado com recheio de presunto e queijo.",
+    price: 8.49,
+  },
+  {
+    id: 16,
+    image: SnackExample,
+    name: "Pão de Queijo",
+    description: "Clássico pão de queijo brasileiro, macio e delicioso.",
+    price: 3.99,
+  },
+];
+
+export const SweetMock = [
+  {
+    id: 17,
+    image: SweetExample,
+    name: "Brigadeiro",
+    description: "Doce brasileiro de chocolate coberto com granulado.",
+    price: 2.49,
+  },
+  {
+    id: 18,
+    image: SweetExample,
+    name: "Beijinho",
+    description: "Doce de coco coberto com açúcar e um cravo no topo.",
+    price: 2.49,
+  },
+  {
+    id: 19,
+    image: SweetExample,
+    name: "Torta de Limão",
+    description: "Torta com creme de limão e merengue por cima.",
+    price: 7.99,
+  },
+  {
+    id: 20,
+    image: SweetExample,
+    name: "Bolo de Cenoura",
+    description: "Bolo de cenoura com cobertura de chocolate.",
+    price: 6.99,
+  },
+  {
+    id: 21,
+    image: SweetExample,
+    name: "Churros",
+    description: "Churros recheados com doce de leite.",
+    price: 5.49,
+  },
+  {
+    id: 22,
+    image: SweetExample,
+    name: "Pudim",
+    description: "Pudim de leite condensado com calda de caramelo.",
+    price: 8.99,
+  },
+  {
+    id: 23,
+    image: SweetExample,
+    name: "Tiramisu",
+    description: "Clássica sobremesa italiana com café e mascarpone.",
+    price: 12.49,
+  },
+  {
+    id: 24,
+    image: SweetExample,
+    name: "Brownie",
+    description: "Brownie de chocolate com pedaços de nozes.",
+    price: 6.49,
+  },
+];
+
+export const DrinkMock = [
+  {
+    id: 25,
+    image: DrinkExample,
+    name: "Coca-Cola",
+
+    price: 4.99,
+  },
+  {
+    id: 26,
+    image: DrinkExample,
+    name: "Pepsi",
+
+    price: 4.99,
+  },
+  {
+    id: 27,
+    image: DrinkExample,
+    name: "Guaraná",
+
+    price: 4.49,
+  },
+  {
+    id: 28,
+    image: DrinkExample,
+    name: "Água com Gás",
+
+    price: 3.49,
+  },
+  {
+    id: 29,
+    image: DrinkExample,
+    name: "Água Mineral",
+
+    price: 2.99,
+  },
+  {
+    id: 30,
+    image: DrinkExample,
+    name: "Suco de Laranja",
+
+    price: 5.99,
+  },
+  {
+    id: 31,
+    image: DrinkExample,
+    name: "Suco de Uva",
+
+    price: 5.99,
+  },
+  {
+    id: 32,
+    image: DrinkExample,
+    name: "Chá Gelado",
+
+    price: 4.99,
   },
 ];
