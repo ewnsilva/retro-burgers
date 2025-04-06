6a31b17c014fc4092102d6ea9eb259ca0a86aceb
Ericles Willian <ewnsilva@gmail.com>
2025-04-06 20:08:10 -0300

    style: code formatting

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,21 +1,14 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { Box, Grid2 as Grid } from "@mui/material";
+import { Box, Grid2 as Grid } from '@mui/material';
 
-import {
-  Cart,
-  Header,
-  Navigation,
-  ProductCard,
-  CartButton,
-  Footer,
-} from "components";
-import { useCart } from "hooks";
-import { BurguerMock, DrinkMock, SnackMock, SweetMock } from "utils";
+import { Cart, Header, Navigation, ProductCard, CartButton, Footer } from 'components';
+import { useCart } from 'hooks';
+import { BurguerMock, DrinkMock, SnackMock, SweetMock } from 'utils';
 
 export const Home = () => {
   const { totalQuantity } = useCart();
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
   const [category, setCategory] = useState(0);
 
   const getMockByNumber = (number: number) => {
@@ -33,23 +26,18 @@ export const Home = () => {
     }
   };
 
-  const filteredProducts = getMockByNumber(category).filter((product) =>
+  const filteredProducts = getMockByNumber(category).filter(product =>
     product.name.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
-    <Box display="flex" flexDirection={"column"}>
+    <Box display="flex" flexDirection={'column'}>
       <Header setSearch={setSearch} />
       <Navigation setCategory={setCategory} categoty={category} />
       {totalQuantity > 0 && <CartButton />}
       <Cart />
-      <Grid
-        container
-        rowSpacing={2}
-        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        marginX={"10%"}
-      >
-        {filteredProducts.map((item) => (
+      <Grid container rowSpacing={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }} marginX={'10%'}>
+        {filteredProducts.map(item => (
           <Grid key={item.id} size={3}>
             <ProductCard item={item} />
           </Grid>
