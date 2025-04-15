b6cddec1308fe011b3323a2dcf304af67b42049a
Ericles Willian <ewnsilva@gmail.com>
2025-04-14 23:18:30 -0300

    style: prettier formatting

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,6 +1,6 @@
-import BurguerExample from "./Images/BurguerExample.jpg";
-import DrinkExample from "./Images/DrinkExample.jpg";
-import SnackExample from "./Images/SnackExample.jpg";
-import SweetExample from "./Images/SweetExample.jpg";
+import BurguerExample from './Images/BurguerExample.jpg';
+import DrinkExample from './Images/DrinkExample.jpg';
+import SnackExample from './Images/SnackExample.jpg';
+import SweetExample from './Images/SweetExample.jpg';
 
 export { BurguerExample, DrinkExample, SnackExample, SweetExample };
