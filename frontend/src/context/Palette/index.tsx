8c000d8b16c040fe7252930253a5666f55181764
Ericles Willian <ewnsilva@gmail.com>
2025-04-16 23:07:57 -0300

    refactor: updating defaultTheme colors

--- frontend/src/context/Palette/index.tsx
+++ frontend/src/context/Palette/index.tsx
@@ -1,30 +1,30 @@
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material';
 
 export const defaultTheme = createTheme({
   palette: {
     primary: {
-      main: "#FA558A",
-      contrastText: "#ffffff",
+      main: '#fa558a',
+      contrastText: '#ffffff',
     },
     secondary: {
-      main: "#feae5d",
-      contrastText: "#e5e5e5",
+      main: '#feae5d',
+      contrastText: '#e5e5e5',
     },
     info: {
-      main: "#fff4e0",
-      contrastText: "black",
+      main: '#fff4e0',
+      contrastText: 'black',
     },
     background: {
-      default: "#250f40",
+      default: '#250f40',
     },
   },
   typography: {
-    fontFamily: "arial",
+    fontFamily: 'arial',
     h1: {
-      color: "#e5e5e5",
+      color: '#e5e5e5',
     },
     h2: {
-      color: "#e5e5e5",
+      color: '#e5e5e5',
     },
   },
 });
