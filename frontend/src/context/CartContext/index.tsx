6393e5193f5380cfdb512a31df506a5f6ac7b74e
Ericles Willian <ewnsilva@gmail.com>
2025-04-18 08:23:25 -0300

    style: prettier formatting

--- frontend/src/context/CartContext/index.tsx
+++ frontend/src/context/CartContext/index.tsx
@@ -1,6 +1,6 @@
-import { IProducts } from "context";
-import React, { createContext, ReactNode, useState } from "react";
-import { formatPrice } from "utils";
+import { IProducts } from 'context';
+import React, { createContext, ReactNode, useState } from 'react';
+import { formatPrice } from 'utils';
 
 interface CartProductsProps extends IProducts {
   totalPrice?: number;
@@ -21,67 +21,57 @@ export interface CartContextProps {
   decrementQuantity: (id: number) => void;
 }
 
-export const CartContext = createContext<CartContextProps>(
-  {} as CartContextProps
-);
+export const CartContext = createContext<CartContextProps>({} as CartContextProps);
 
-export const CartProvider: React.FC<{ children: ReactNode }> = ({
-  children,
-}) => {
+export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [cartItems, setCartItems] = useState<CartProductsProps[]>([]);
 
   const removeItem = (id: number): void => {
-    setCartItems((prevState) => prevState.filter((item) => item.id !== id));
+    setCartItems(prevState => prevState.filter(item => item.id !== id));
   };
 
   const updatePrice = (id: number, quantity: number): void => {
-    setCartItems((prevState) =>
-      prevState.map((item) =>
-        item.id === id
-          ? { ...item, quantity, totalPrice: quantity * item.price }
-          : item
+    setCartItems(prevState =>
+      prevState.map(item =>
+        item.id === id ? { ...item, quantity, totalPrice: quantity * item.price } : item
       )
     );
   };
 
   const addToCart = (item: CartProductsProps) => {
-    const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
+    const existingItem = cartItems.find(cartItem => cartItem.id === item.id);
     if (!existingItem) {
       const newItem = { ...item, quantity: 1, totalPrice: item.price };
-      setCartItems((prevState) => [...prevState, newItem]);
+      setCartItems(prevState => [...prevState, newItem]);
     } else {
       updatePrice(item.id, existingItem.quantity! + 1);
     }
   };
 
-  const totalQuantity = cartItems.reduce(
-    (sum, item) => sum + (item.quantity || 0),
-    0
-  );
+  const totalQuantity = cartItems.reduce((sum, item) => sum + (item.quantity || 0), 0);
 
   const updateValue = () => {
     const total = cartItems.reduce(
-      (previousValue, currentValue) =>
-        previousValue + (currentValue.totalPrice || 0),
+      (previousValue, currentValue) => previousValue + (currentValue.totalPrice || 0),
       0
     );
     return formatPrice(total);
   };
 
   const isInCart = (id: number) => {
-    return cartItems.some((item) => item.id === id);
+    return cartItems.some(item => item.id === id);
   };
 
   const incrementQuantity = (id: number): void => {
-    const existingItem = cartItems.find((item) => item.id === id);
+    const existingItem = cartItems.find(item => item.id === id);
     if (existingItem) {
       updatePrice(id, existingItem.quantity! + 1);
     }
   };
 
   const decrementQuantity = (id: number): void => {
-    const existingItem = cartItems.find((item) => item.id === id);
+    const existingItem = cartItems.find(item => item.id === id);
     if (existingItem) {
       if (existingItem.quantity === 1) {
         removeItem(id);
