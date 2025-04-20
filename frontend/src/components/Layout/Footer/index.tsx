3694a7b50239e49f154811cf1c42210955bfe37a
Ericles Willian <ewnsilva@gmail.com>
2025-04-20 07:33:44 -0300

    refactor: using colors from theme palette

--- frontend/src/components/Layout/Footer/index.tsx
+++ frontend/src/components/Layout/Footer/index.tsx
@@ -1,20 +1,21 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography } from '@mui/material';
 
 export const Footer = (): JSX.Element => {
   return (
-    <footer
-      style={{
-        width: "100%",
-        display: "flex",
-        justifyContent: "space-between",
-        backgroundColor: "#FA558A",
+    <Box
+      component="footer"
+      sx={{
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        backgroundColor: 'primary.main',
       }}
     >
-      <Box display="flex" ml={"2%"} paddingY={1}>
-        <Typography variant="h6" ml={1} sx={{ color: "white" }}>
+      <Box display="flex" ml="2%" paddingY={1}>
+        <Typography variant="h6" ml={1} sx={{ color: 'white' }}>
           © 2024 Retro-Burguers. Todos os direitos reservados
         </Typography>
       </Box>
-    </footer>
+    </Box>
   );
 };
