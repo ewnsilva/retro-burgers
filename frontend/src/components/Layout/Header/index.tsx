f4176f557a33ccbbb80c7b088726df165e259a19
Ericles Willian <ewnsilva@gmail.com>
2025-04-20 07:33:59 -0300

    refactor: using colors from theme palette

--- frontend/src/components/Layout/Header/index.tsx
+++ frontend/src/components/Layout/Header/index.tsx
@@ -1,37 +1,39 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent } from 'react';
 
-import { Fastfood, Search } from "@mui/icons-material";
-import { Box, InputAdornment, TextField, Typography } from "@mui/material";
+import { Box, InputAdornment, TextField, Typography, useTheme } from '@mui/material';
+import { Fastfood, Search } from '@mui/icons-material';
 
-import { IHeader } from "context";
+import { IHeader } from 'context';
 
 export const Header = ({ setSearch }: IHeader): JSX.Element => {
+  const theme = useTheme();
   const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearch(event.target.value);
   };
 
   return (
-    <header
-      style={{
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        backgroundColor: "#fff4e0",
+    <Box
+      component="header"
+      sx={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: 'info.main',
       }}
     >
-      <Box display="flex" ml={"2%"} paddingY={2}>
-        <Fastfood htmlColor="#FA558A" fontSize="large" />
+      <Box display="flex" ml="2%" paddingY={2}>
+        <Fastfood htmlColor={theme.palette.primary.main} fontSize="large" />
         <Typography
           variant="h4"
           ml={1}
           sx={{
-            backgroundImage: `radial-gradient(50% 50% at 50% 50%, #250F40 0%, #FA558A 100%)`,
-            WebkitBackgroundClip: "text",
-            WebkitTextFillColor: "transparent",
-            fontWeight: "bold",
-            display: "inline-block",
+            backgroundImage: `radial-gradient(50% 50% at 50% 50%, ${theme.palette.background.default} 0%, ${theme.palette.primary.main} 100%)`,
+            WebkitBackgroundClip: 'text',
+            WebkitTextFillColor: 'transparent',
+            fontWeight: 'bold',
+            display: 'inline-block',
           }}
         >
           Retro-Burgers
@@ -49,8 +51,8 @@ export const Header = ({ setSearch }: IHeader): JSX.Element => {
             </InputAdornment>
           ),
         }}
-        sx={{ mr: "5%" }}
+        sx={{ mr: '5%' }}
       />
-    </header>
+    </Box>
   );
 };
