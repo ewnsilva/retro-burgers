d8aeb48fda56dbe631b088201fd5ba14a4a7d748
Ericles Willian <ewnsilva@gmail.com>
2025-04-23 21:37:13 -0300

    refactor: updating imports directories

--- frontend/src/utils/hooks/products/useCart.ts
+++ frontend/src/utils/hooks/products/useCart.ts
@@ -1,4 +1,4 @@
 import { useContext } from 'react';
-import { CartContext, CartContextProps } from 'context';
+import { CartContext, CartContextProps } from 'utils';
 
 export const useCart = (): CartContextProps => useContext(CartContext);
