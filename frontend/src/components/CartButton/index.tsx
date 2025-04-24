805a3e5648ae1e6fb2fc3a45052cb5490a030d8c
Ericles Willian <ewnsilva@gmail.com>
2025-04-23 21:37:59 -0300

    refactor: updating import directory

--- frontend/src/components/CartButton/index.tsx
+++ frontend/src/components/CartButton/index.tsx
@@ -1,7 +1,7 @@
 import { ShoppingCart } from '@mui/icons-material';
 import { Badge, IconButton } from '@mui/material';
 
-import { useCart } from 'hooks';
+import { useCart } from 'utils';
 
 export const CartButton = (): JSX.Element => {
   const { setIsDrawerOpen, totalQuantity } = useCart();
