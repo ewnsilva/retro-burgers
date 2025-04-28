e949f547dbf46bb2f386bae758e2b8ba342de791
Ericles Willian <ewnsilva@gmail.com>
2025-04-28 19:44:13 -0300

    delete: removing not used file

--- frontend/src/utils/assets/index.ts
+++
@@ -1,6 +0,0 @@
-import BurguerExample from './Images/BurguerExample.jpg';
-import DrinkExample from './Images/DrinkExample.jpg';
-import SnackExample from './Images/SnackExample.jpg';
-import SweetExample from './Images/SweetExample.jpg';
-
-export { BurguerExample, DrinkExample, SnackExample, SweetExample };
