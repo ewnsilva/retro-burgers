329ff3f41fc424ae7c2bba99ced164c54648494b
Max Milan Campos de Oliveira <maxmilan10@hotmail.com>
2025-05-16 03:28:30 -0300

    feat: category association with product

--- backend/src/app/Modules/Category/Models/category.ts
+++ backend/src/app/Modules/Category/Models/category.ts
@@ -1,7 +1,14 @@
 import { Model, DataTypes, Sequelize } from "sequelize";
 import { CategoryAttributes } from "../Interfaces/Category.interfaces";
+import { Product } from "modules/Product/Models/product";
 
-export class Category extends Model<CategoryAttributes> {}
+export class Category extends Model<CategoryAttributes> {
+  static associate() {
+    Category.hasMany(Product, {
+      foreignKey: "category_id",
+    });
+  }
+}
 
 export const CategoryModel = (sequelize: Sequelize) => {
   Category.init(
