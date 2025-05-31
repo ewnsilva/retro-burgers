fbdfda369d12edfc6a8041dee76b64f79bbdaf65
Ericles Willian <ewnsilva@gmail.com>
2025-05-31 07:56:09 -0300

    refactor: replacing mock with hook

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,30 +3,16 @@ import { useState } from 'react';
 import { Box, Grid2 as Grid } from '@mui/material';
 
 import { Cart, Header, Navigation, ProductCard, CartButton, Footer } from 'components';
-import { useCart } from 'hooks';
-import { BurguerMock, DrinkMock, SnackMock, SweetMock } from 'utils';
+import { useCart, useProducts } from 'utils';
 
 export const Home = () => {
   const { totalQuantity } = useCart();
+  const { products } = useProducts();
+
   const [search, setSearch] = useState('');
   const [category, setCategory] = useState(0);
 
-  const getMockByNumber = (number: number) => {
-    switch (number) {
-      case 0:
-        return BurguerMock;
-      case 1:
-        return SnackMock;
-      case 2:
-        return SweetMock;
-      case 3:
-        return DrinkMock;
-      default:
-        return BurguerMock;
-    }
-  };
-
-  const filteredProducts = getMockByNumber(category).filter(product =>
+  const filteredProducts = products.filter(product =>
     product.name.toLowerCase().includes(search.toLowerCase())
   );
 
