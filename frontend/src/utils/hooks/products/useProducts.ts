c6b6c24fb21d0f8adabc393f1b58bc85d424365c
Ericles Willian <ewnsilva@gmail.com>
2025-05-31 07:49:45 -0300

    refactor: updating hook for list products

--- frontend/src/utils/hooks/products/useProducts.ts
+++ frontend/src/utils/hooks/products/useProducts.ts
@@ -1,4 +1,20 @@
+import { useState } from 'react';
+
+import { IProducts, useAxios } from 'utils';
+
 export const useProducts = () => {
-  const teste = console.log('teste');
-  return { teste };
+  const { api } = useAxios();
+  const [products, setProducts] = useState<IProducts[]>([{} as IProducts]);
+
+  const fetchProducts = (category: number) => {
+    const url = `www.api.com/${category}`;
+    api
+      .get(url)
+      .then(({ data }) => {
+        setProducts(data);
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  return { fetchProducts, products };
 };
