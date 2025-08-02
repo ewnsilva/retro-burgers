346beff57268fdc70f019eba888454462afb375f
Ericles Willian <ewnsilva@gmail.com>
2025-08-02 19:05:35 -0300

    refactor: adding new themes

---
+++ frontend/src/utils/context/Theme/Palette/index.tsx
@@ -0,0 +1,88 @@
+import { createTheme } from '@mui/material';
+
+export const synthwaveTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#fa558a',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#feae5d',
+      contrastText: '#e5e5e5',
+    },
+    info: {
+      main: '#fff4e0',
+      contrastText: 'black',
+    },
+    background: {
+      default: '#250f40',
+    },
+  },
+  typography: {
+    fontFamily: 'arial',
+    h1: {
+      color: '#e5e5e5',
+    },
+    h2: {
+      color: '#e5e5e5',
+    },
+  },
+});
+
+export const retrowaveTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#1df7fd',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#FEFF50',
+      contrastText: '#e5e5e5',
+    },
+    info: {
+      main: '#fff4e0',
+      contrastText: 'black',
+    },
+    background: {
+      default: '#250f40',
+    },
+  },
+  typography: {
+    fontFamily: 'arial',
+    h1: {
+      color: '#e5e5e5',
+    },
+    h2: {
+      color: '#e5e5e5',
+    },
+  },
+});
+
+export const darkwaveTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#fd1687',
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: '#5123F4',
+      contrastText: '#e5e5e5',
+    },
+    info: {
+      main: '#fff4e0',
+      contrastText: 'black',
+    },
+    background: {
+      default: '#250f40',
+    },
+  },
+  typography: {
+    fontFamily: 'arial',
+    h1: {
+      color: '#e5e5e5',
+    },
+    h2: {
+      color: '#e5e5e5',
+    },
+  },
+});
