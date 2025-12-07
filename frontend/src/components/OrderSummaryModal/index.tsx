cbdf8926717198f5b0817a4ccbea7a1c3c0186ba
Ericles Willian <ewnsilva@gmail.com>
2025-12-06 21:10:08 -0300

    feat: adding order summary modal

--- frontend/src/components/OrderSummaryModal/index.tsx
+++ frontend/src/components/OrderSummaryModal/index.tsx
@@ -1,4 +1,3 @@
-// components/OrderSummaryModal.tsx
 import {
   Dialog,
   DialogTitle,
@@ -21,24 +20,34 @@ export const OrderSummaryModal = ({ open, onClose, onConfirm }: Props) => {
   const { cartItems, updateValue } = useCart();
 
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
-      <DialogTitle variant="h5" fontWeight={700}>
+    <Dialog
+      open={open}
+      onClose={onClose}
+      maxWidth="sm"
+      fullWidth
+      sx={{
+        '& .MuiPaper-root': {
+          backgroundColor: 'background.default',
+        },
+      }}
+    >
+      <DialogTitle variant="h5" fontWeight={700} color="primary">
         Resumo do Pedido
       </DialogTitle>
 
       <DialogContent dividers>
         {cartItems.map(item => (
           <Box key={item.id} display="flex" justifyContent="space-between" mb={1}>
-            <Typography fontWeight={600}>
+            <Typography fontWeight={600} color="info">
               {item.name} x{item.quantity}
             </Typography>
-            <Typography color="primary">R$ {item.price * Number(item.quantity)}</Typography>
+            <Typography color="info">R$ {item.price * Number(item.quantity)}</Typography>
           </Box>
         ))}
 
         <Divider sx={{ mt: 2, mb: 1 }} />
 
-        <Typography variant="h6" fontWeight={700} textAlign="right">
+        <Typography variant="h6" fontWeight={700} textAlign="right" color="primary">
           Total: {updateValue()}
         </Typography>
       </DialogContent>
