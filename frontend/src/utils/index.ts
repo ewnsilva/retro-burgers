80f94a35a1051c667515c198a08b853edd25384a
Ericles Willian <ewnsilva@gmail.com>
2025-12-13 20:40:53 -0300

    refactor: hook for translation

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -2,3 +2,4 @@ export * from './constants/formatPrice';
 export * from './context';
 export * from './hooks';
 export * from './interfaces';
+export * from './translation';
---
+++ frontend/src/utils/translation/index.ts
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import pt from './pt.json';
+import en from './en.json';
+
+const savedLanguage = localStorage.getItem('retroburguers-language') || 'en';
+
+i18n.use(initReactI18next).init({
+  resources: {
+    pt: { translation: pt },
+    en: { translation: en },
+  },
+  lng: savedLanguage,
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
