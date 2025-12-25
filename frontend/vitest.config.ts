a1088f848a1c08b294b79421a395397c44239974
Ericles Willian <ewnsilva@gmail.com>
2025-12-25 19:22:32 -0300

    refactor: updating vitest settings

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -11,9 +11,10 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      '@': path.resolve(__dirname, 'src'),
       components: path.resolve(__dirname, 'src/components'),
-      pages: path.resolve(__dirname, 'src/pages'),
       utils: path.resolve(__dirname, 'src/utils'),
+      pages: path.resolve(__dirname, 'src/pages'),
     },
   },
 });
