dc469a49f06478bf5c613e4dfb8ab0b839c50147
Ericles Willian <ewnsilva@gmail.com>
2026-01-10 19:19:06 -0300

    refactor: adding cors settings

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,17 @@
 import express from "express";
+import cors from "cors";
+import sequelize from "./database";
 import { routes } from "shared/Routes/Routes";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error("Database error:", err));
 
 routes(app);
 
