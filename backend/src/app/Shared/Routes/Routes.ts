cab391b8880773b681a844193be344b71919e9f3
Ericles Willian <ewnsilva@gmail.com>
2026-01-10 19:19:59 -0300

    refactor: update due new enpoint

--- backend/src/app/Shared/Routes/Routes.ts
+++ backend/src/app/Shared/Routes/Routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { categoryRouter } from "modules/Category/Routes/category.routes";
 import { productRouter } from "modules/Product/Routes/product.routes";
+import { additionalRouter } from "modules/Additionals/Routes/additional.routes";
 
 export const routes = (app: express.Application) => {
   app.use(express.json());
   app.use(categoryRouter);
   app.use(productRouter);
+  app.use(additionalRouter);
 };
