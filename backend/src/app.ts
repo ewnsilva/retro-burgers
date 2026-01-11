38aefa6371db187760d6d28640a612a54cc6db31
Ericles Willian <ewnsilva@gmail.com>
2026-01-11 15:50:20 -0300

    refactor: considering new directories

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import cors from "cors";
 import sequelize from "./database";
@@ -7,6 +8,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/images", express.static(path.resolve(__dirname, "../public/images")));
+app.use("/audio", express.static(path.resolve(__dirname, "../public/audio")));
 
 sequelize
   .authenticate()
