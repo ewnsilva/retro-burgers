57e908afe08d1e34cce31ea8b10968f95dcca78c
Ericles Willian <ewnsilva@gmail.com>
2026-01-22 20:33:30 -0300

    refactor: adding QueryClientProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,27 @@
 import { BrowserRouter } from 'react-router-dom';
-
 import { CssBaseline } from '@mui/material';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 import { AppRoutes } from 'utils/routes';
 import { CartProvider } from 'utils/context/CartContext';
 import { ThemeProvider } from 'utils/context/Theme';
+import { queryClient } from 'utils/hooks/useQueryClient';
 
 import './App.css';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <CssBaseline />
-        <CartProvider>
-          <div className="App">
-            <AppRoutes />
-          </div>
-        </CartProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider>
+          <CssBaseline />
+          <CartProvider>
+            <div className="App">
+              <AppRoutes />
+            </div>
+          </CartProvider>
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
