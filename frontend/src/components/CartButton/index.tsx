dfa8ad1993d61e38b09e4859a6483aabdedd029c
Ericles Willian <ewnsilva@gmail.com>
2026-02-01 10:34:28 -0300

    refactor: style adjustments

--- frontend/src/components/CartButton/index.tsx
+++ frontend/src/components/CartButton/index.tsx
@@ -1,11 +1,12 @@
 import { ShoppingCart } from '@mui/icons-material';
-import { Badge, IconButton, useTheme } from '@mui/material';
+import { Badge, IconButton, useMediaQuery, useTheme } from '@mui/material';
 
 import { useCart } from 'utils/hooks/products/useCart';
 
 export const CartButton = (): JSX.Element => {
   const theme = useTheme();
   const { setIsDrawerOpen, totalQuantity } = useCart();
+  const matchesXs = useMediaQuery('(max-width: 465px)');
 
   const openDrawer = () => {
     setIsDrawerOpen(true);
@@ -16,7 +17,9 @@ export const CartButton = (): JSX.Element => {
       onClick={openDrawer}
       sx={{
         position: 'fixed',
-        top: '20%',
+        border: matchesXs ? `1px solid ${theme.palette.secondary.main}` : null,
+        background: matchesXs ? `${theme.palette.background.default}` : null,
+        top: '30%',
         right: 16,
         transition: '0.25s',
         '&:hover': {
@@ -28,6 +31,17 @@ export const CartButton = (): JSX.Element => {
       <Badge
         badgeContent={totalQuantity}
         sx={{
+          '& .MuiBadge-badge': {
+            top: matchesXs ? -5 : 'none',
+            right: matchesXs ? -10 : 'none',
+            border: matchesXs ? `1px solid ${theme.palette.secondary.main}` : 'none',
+            backgroundColor: matchesXs ? theme.palette.background.default : 'none',
+            color: theme.palette.secondary.main,
+            fontSize: matchesXs ? '0.75rem' : '0.8rem',
+            minWidth: matchesXs ? 22 : 18,
+            height: matchesXs ? 22 : 18,
+            fontWeight: 600,
+          },
           color: `${theme.palette.secondary.main}`,
           transition: '0.25s',
         }}
